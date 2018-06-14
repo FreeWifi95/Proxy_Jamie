ff4f05c212c53534815edf894041e70aec075cda
Eunbyul Cho <hello@eunbyulcho.me>
2018-06-14 15:50:34 -0700

    delete test file

--- proxy/server.js
+++ proxy/server.js
@@ -6,10 +6,9 @@ const port = process.env.PORT || 3000;
 const request = require('request');
 
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/:id', express.static(path.join(__dirname, 'public')));
 
 app.get("/house/:houseId/",(req,res) =>  {
-  console.log('houseIdFrom,',req.params)
   request(`http://localhost:3007/house/${req.params.houseId}`,function(err,response,body) {
     if (err) {
       res.sendStatus(500);
@@ -48,7 +47,17 @@ app.get('/user/:userId',(req,res) =>  {
 });
 
 app.get('/dates/:id', (req, res) => {
+  console.log('proxyxyxyxy')
   request(`http://localhost:3002/dates/${req.params.id}`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  })
+})
+app.get('/info/:id', (req, res) => {
+  request(`http://localhost:3002/info/${req.params.id}`,function(err,response,body) {
     if (err) {
       res.sendStatus(500);
     } else {
