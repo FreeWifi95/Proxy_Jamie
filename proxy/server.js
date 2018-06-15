e4eb1063696cd859c33f71112364533320795783
Eunbyul Cho <hello@eunbyulcho.me>
2018-06-15 14:21:16 -0700

    render all component(need to fix css)

--- proxy/server.js
+++ proxy/server.js
@@ -7,17 +7,8 @@ const request = require('request');
 
 app.use(morgan('dev'));
 app.use('/:id', express.static(path.join(__dirname, 'public')));
-/*
-app.get("/photos/:id",(req,res) =>  {
-  request(`http://localhost:3003/photos/${req.params.id}`,function(err,response,body) {
-    if (err) {
-      res.sendStatus(500);
-    } else {
-      res.send(body);
-    }
-  })
-});
-*/
+
+
 app.get('/photos/:id', (req, res) => {
   request(`http://localhost:3003/photos/${req.params.id}`, function (error, response, body) {
     if (!error && response.statusCode === 200) {
@@ -72,7 +63,7 @@ app.get('/dates/:id', (req, res) => {
       res.send(body);
     }
   })
-})
+});
 app.get('/info/:id', (req, res) => {
   request(`http://localhost:3002/info/${req.params.id}`,function(err,response,body) {
     if (err) {
@@ -81,7 +72,63 @@ app.get('/info/:id', (req, res) => {
       res.send(body);
     }
   })
-})
+});
+app.get('/listing/:id', (req, res) => {
+  request(`http://localhost:3009/listing/${req.params.id}`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      console.log(body)
+      res.send(body);
+    }
+  })
+});
+app.get('/lists/:id', (req, res) => {
+  request(`http://localhost:3009/lists/${req.params.id}`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  })
+});
+app.get('/lists2listings/:id', (req, res) => {
+  request(`http://localhost:3009/lists2listings/${req.params.id}`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  })
+});
+app.get('/like/:id', (req, res) => {
+ // console.log('==========',req.query,req,params)
+  request(`http://localhost:3009/like/${req.params.id}`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  })
+});
+app.get('/like', (req, res) => {
+  request(`http://localhost:3009/like`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  })
+});
+app.post('/lists', (req, res) => {
+  request(`http://localhost:3009/lists`,function(err,response,body) {
+    if (err) {
+      res.sendStatus(500);
+    } else {
+      res.send(body);
+    }
+  })
+});
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });
